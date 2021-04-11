1b8588de3e831a46139469b7e4abe0dc7f17bc5e
Eladio Rodriguez <eladiojuniorrodriguez@gmail.com>
2021-04-10 21:27:01 -0400

    Pre last change

---
+++ src/pages/Login.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { signin, signInWithGoogle, signInWithGitHub } from "../helpers/auth";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      email: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.googleSignIn = this.googleSignIn.bind(this);
+    this.githubSignIn = this.githubSignIn.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ error: "" });
+    try {
+      await signin(this.state.email, this.state.password);
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  async googleSignIn() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  async githubSignIn() {
+    try {
+      await signInWithGitHub();
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form
+          className="mt-5 py-5 px-5"
+          autoComplete="off"
+          onSubmit={this.handleSubmit}
+        >
+          <h1>
+            Iniciar sesión en{" "}
+            <Link className="title ml-2" to="/">
+              Chat App
+            </Link>
+          </h1>
+          <p className="lead">
+            Llena el campo debajo para iniciar sesión en tu cuenta.
+          </p>
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="Clave"
+              name="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+              type="password"
+            />
+          </div>
+          <div className="form-group">
+            {this.state.error ? (
+              <p className="text-danger">{this.state.error}</p>
+            ) : null}
+            <button className="btn btn-danger mr-2" type="submit">
+              Iniciar Sesion
+            </button>
+          </div>
+          <p>Puedes iniciar sesion con algunos de estos servicios</p>
+          <button
+            className="btn btn-danger mr-2"
+            type="button"
+            onClick={this.googleSignIn}
+          >
+            Iniciar Sesion con Google
+          </button>
+          <button
+            className="btn btn-secondary"
+            type="button"
+            onClick={this.githubSignIn}
+          >
+            Iniciar Sesion con GitHub
+          </button>
+          <hr />
+          <p>
+            ¿No tienes una cuenta? <Link to="/signup"> Regustrarse </Link>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,1 +1,11 @@
-import firebase from 'firebase';
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyCpbWOyfeV1INiy1Gc2F6caojDs8IWeyTk",
+  authDomain: "chatapp-fa0bf.firebaseapp.com",
+  databaseURL: "https://chatapp-fa0bf-default-rtdb.firebaseio.com/",
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth;
+export const db = firebase.database();
